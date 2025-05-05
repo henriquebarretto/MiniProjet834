be5abe31eb91ab9ac42e55561410a07c6dfb330a
francicardoso <80334463+francicardoso@users.noreply.github.com>
2025-05-05 15:32:23 +0200

    changes before merge
    needed the informations on the branch mongodb

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,44 @@
-# main.py
+# backend/app/main.py
+
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from app.utils.redis_manager import add_user_online, remove_user_online
+from app.routes import routers
+from app.db.connection import Connection
+
+
+app = FastAPI()
+
+#store active WebSocket connections
+connections = {}
+
+#initialize MongoDB and Redis 
+@app.on_event("startup")
+def startup_event():
+    connection = Connection()
+    app.state.mongo_db = connection.get_mongo_db()
+    print(f"Connected to MongoDB: {app.state.mongo_db.name}")
+    
+    app.state.redis_client = connection.get_redis_client()
+    print("Connected to Redis")
+
+#webSocket endpoint for real-time communication
+@app.websocket("/ws/{username}")
+async def websocket_endpoint(websocket: WebSocket, username: str):
+    await websocket.accept()
+    connections[username] = websocket
+    add_user_online(username)
+
+    try:
+        while True:
+            data = await websocket.receive_text()
+            #handle incoming message 
+    except WebSocketDisconnect:
+        connections.pop(username, None)
+        remove_user_online(username)
+
+
+for router in routers:
+    app.include_router(router)
+
+#command to run with uvicon
+#uvicorn backend.app.main:app --reload
